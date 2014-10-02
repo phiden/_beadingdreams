abdd54c41439b8627e7f800550b958fbc76e129a
Sophia Dengo <sdengo@gmail.com>
2014-10-02 15:49:31 -0700

    updated plugins; changed homepage loops and tweaked sidebar, esp. paypal

--- wp-content/themes/Beading Dreams/page-welcome.php
+++ wp-content/themes/Beading Dreams/page-welcome.php
@@ -10,9 +10,6 @@
  *
  * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
  *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
@@ -21,70 +18,77 @@
 
 <div id="main_content">
 
-<div id="home_banner">
-
-	<?php query_posts('cat=22&posts_per_page=1') ?>
-	<?php while ( have_posts() ) : the_post(); ?>
+	<section id="announcements">
 	
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
-
-	<?php endwhile; ?>
-
-</div>
-
-<?php wp_reset_query(); ?>
-
-<div id="home_featured_class">
-
-	<?php query_posts('cat=11&posts_per_page=1') ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
-
-	<?php endwhile; ?>
-
-</div>
-
-<?php wp_reset_query(); ?>
-
-<div class="home_columns">
-
-<ul id="home_announcements">
-
-	<?php query_posts('cat=7&posts_per_page=3') ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-
-	<li>
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
-	</li>
-
-	<?php endwhile; ?>
+		Announcements here
+		<?php query_posts('category_name=home-banner&posts_per_page=1') ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
 	
-</ul>
-<?php wp_reset_query(); ?>
+		<?php endwhile; ?>
+		
+	</section>
 
-</div>
-
-<div class="home_columns">
-
-<ul id="home_classes">
-
-	<?php query_posts('cat=9&posts_per_page=3') ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-
-	<li>
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
-	</li>
-
-	<?php endwhile; ?>
+	<section id="events">
 	
-</ul>
-<?php wp_reset_query(); ?>
-
-</div>
+		Events here 
+		<?php query_posts('category_name=upcoming-event&posts_per_page=1') ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
+	
+		<?php endwhile; ?>
+		
+	</section>
+	
+	<section id="classes">
+	
+		classes here
+	
+		<ul id="home_classes">
+			<?php query_posts('category_name=homepage-class&posts_per_page=3') ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<li>
+				<h1><?php the_title() ?></h1>
+				<?php the_content() ?>
+			</li>
+			<?php endwhile; ?>
+			
+		</ul>
+	</section>
+	
+	<section id="design-repair">
+	
+		Design &amp; repair here
+		<?php query_posts('category_name=design-repair&posts_per_page=1') ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
+	
+		<?php endwhile; ?>
+		
+	</section>
+	
+	
+	<div id="home_banner">
+	
+		<?php query_posts('cat=home-banner&posts_per_page=1') ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+		<h1><?php the_title() ?></h1>
+		<?php the_content() ?>
+	
+		<?php endwhile; ?>
+	
+	</div>
+	
+	<?php wp_reset_query(); ?>
+	
 </div><!-- end main_content-->
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
--- wp-content/themes/Beading Dreams/sidebar.php
+++ wp-content/themes/Beading Dreams/sidebar.php
@@ -8,63 +8,61 @@
 	Sun, 1p - 6p</p>
 	</div>
 	
-	<div class="sidebar_section">
+	<div class="sidebar_section" id='gift-certificates'>
 	
-	<h1>Gift Certificates</h1>
-	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-	<input type="hidden" name="cmd" value="_s-xclick">
-	<input type="hidden" name="hosted_button_id" value="43GR9GYJYCXBJ">
-	<table>
-	<tr><td><input type="hidden" name="on0" value="Amounts"></td></tr><tr><td><select name="os0">
-		<option value="Gift certificate">Gift certificate $25.00 USD</option>
-		<option value="Gift certificate">Gift certificate $50.00 USD</option>
-		<option value="Gift certificate">Gift certificate $75.00 USD</option>
-		<option value="Gift certificate">Gift certificate $100.00 USD</option>
-	</select> </td></tr>
-	</table>
-	<input type="hidden" name="currency_code" value="USD">
-	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-	<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-	</form>
-	
+		<h1>Gift Certificates</h1>
+		
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="43GR9GYJYCXBJ">
+		<table id="paypal">
+		<tr><td><input type="hidden" name="on0" value="Choose your amount" id='paypal-label'>Choose your amount</td></tr><tr><td><select id='paypal select' name="os0">
+			<option value="$25 Gift">$25.00 USD</option>
+			<option value="$50 Gift">$50.00 USD</option>
+			<option value="$75 Gift">$75.00 USD</option>
+			<option value="$100 Gift">$100.00 USD</option>
+		</select> </td></tr>
+		</table>
+		<input type="hidden" name="currency_code" value="USD">
+		<input type="image" id='paypal-button' src="http://beadingdreams.com/beading-dreams-paypal-btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+
 	</div>
 	
 	<div class="sidebar_section" id='facebook'>
-	<img id='facebook-icon' src="/images/facebook.jpg"/><h1><a id='facebook-link' href="https://www.facebook.com/pages/Beading-Dreams/257101560041">visit us on Facebook</a></h1>
+		<img id='facebook-icon' src="/images/facebook.jpg"/><h1><a id='facebook-link' href="https://www.facebook.com/pages/Beading-Dreams/257101560041">visit us on Facebook</a></h1>
 	</div>
 	
 	<div class="sidebar_section" id="mailing">
-	<h1><a href="http://visitor.constantcontact.com/d.jsp?m=1103500614685&p=oi">join our mailing list &raquo;</a></h1><p>receive all the latest news</p>
+		<h1><a href="http://visitor.constantcontact.com/d.jsp?m=1103500614685&p=oi">join our mailing list &raquo;</a></h1><p>receive all the latest news</p>
 	</div>
 	
 	<div class="sidebar_section">
-	<h1><a href="/class-schedule">view our class calendar &raquo;</a></h1>
+		<h1><a href="/class-schedule">view our class calendar &raquo;</a></h1>
 	</div>
 	
-	<div class="sidebar_section">
+	<!--<div class="sidebar_section">
 	
-	
+	 removed because it's on the homepage now 
 	<h1>upcoming events:</h1>
 	
 	<?php query_posts('cat=15&posts_per_page=3') ?>
 		
 		<?php while ( have_posts() ) : the_post(); ?>
-		<?php the_title() ?>
-		<?php the_content() ?>
-		
+			<?php the_title() ?>
+			<?php the_content() ?>
 		<?php endwhile; ?>
 		
-	</div>
+	</div>-->
 	
 	<div class="sidebar_section">
-	<h1>Bead Bus</h1>
-	<i>mobile birthday parties</i>
-	<img src="images/bead_bus_small.jpg"/>
-	<p>
-	Let the Bead Bus bring the party to you! Birthday parties for kids ages 6 and up in the convenience of your own home or the venue of your choice! 
-	
-	Check the <a href="/parties">Parties</a> page for complete information!
-	</p>
+		<h1>Bead Bus</h1>
+		<i>mobile birthday parties</i>
+		<img src="images/bead_bus_small.jpg"/>
+		<p>
+		Let the Bead Bus bring the party to you! Birthday parties for kids ages 6 and up in the convenience of your own home or the venue of your choice!</p>
+		<p>Check the <a href="/parties">Parties</a> page for complete information!</p>
 	</div>
 
 </div> <!-- end sidebar -->
